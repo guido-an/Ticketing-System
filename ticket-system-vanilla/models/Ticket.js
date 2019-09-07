0ffe268c96ed3defc8be3f4280c39d1d0d5eebe1
Guido <carucciguido@gmail.com>
2019-09-07 20:51:38 +0200

    make it better

---
+++ ticket-system-vanilla/models/Ticket.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const User = require('../models/User')
+
+const ticketSchema = new Schema(
+  {
+    status: String,
+    author: String,
+    title: { type: String, required: true },
+    message: { type: String, required: true },
+    active: Boolean,
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    answers: Array,
+    time: String,
+    picture: Object
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
+) 
+
+const Ticket = mongoose.model("Ticket", ticketSchema);
+module.exports = Ticket;
+
+// var myTicket = new Ticket({
+//   title: "title",
+//   message: "message",
+//   user: "5d2c7791d3194e24321bdf5f"
+// })
+// myTicket.save()
+
---
+++ ticket-system-vanilla/routes/auth.js
@@ -0,0 +1,118 @@
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+const User = require("../models/User");
+const Ticket = require("../models/Ticket");
+var ObjectId = require("mongodb").ObjectID;
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", { message: req.flash("error") });
+});
+
+/******************************
+1) POST login  *********/
+router.post("/login", (req, res) => {
+  let currentUser;
+  User.findOne({ username: req.body.username })
+    .then(user => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "The username doesn't exist."
+        });
+        return;
+      }
+      currentUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(passwordCorrect => {
+      if (passwordCorrect) {
+        req.session.currentUser = currentUser;
+        res.redirect("/auth/private");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+    });
+});
+
+/******************************
+1) USE and GET private  *********/
+router.use("/private", (req, res, next) => {
+  // change this line of code with .env in production!!!!
+  if (req.session.currentUser.username === process.env.admin) { // if the user is the admin go to admin page - 
+    res.redirect("/admin");
+  } else if (req.session.currentUser) { // <== if there's user in the session (user is logged in) go to the next step
+    next();
+  } else {
+    res.redirect("login");
+  }
+});
+
+router.get("/private", (req, res, next) => {
+  const user = User.findOne({ username: req.session.currentUser.username });
+  const tickets = Ticket.find({ user: ObjectId(req.session.currentUser._id)}).sort( { created_at: -1 });
+
+  Promise.all([user, tickets])
+    .then(values => {
+      console.log(values[1])
+      // res.render("auth/private", { values: values });
+      res.redirect("/tickets")
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+/******************************
+3) GET signup  *********/
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+/******************************
+4) POST signup  *********/
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    });
+
+    newUser
+      .save()
+      .then(() => {
+        res.redirect("/");
+      })
+      .catch(err => {
+        res.render("auth/signup", { message: "Something went wrong" });
+      });
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy(err => {
+    res.redirect("login");
+  });
+});
+
+module.exports = router;
